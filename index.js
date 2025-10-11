8321348de8f899b6fb04fa771f151e4272ae2d3f
Md. Habibur Rahman <rhabibur@ghit.com>
2025-10-11 12:41:12 +0600

    created base and added mongodb

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5001;
+
+
+app.use(cors(
+  {
+  origin: '*', 
+  withcredentials: true,
+}
+));
+
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wx3f0no.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // await client.connect();
+
+    const database = client.db("AncientQuest");
+    const artifacts = database.collection("Artifacts");
+
+
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Artifacts Management server is running')
+})
+
+app.listen(port, () => {
+  console.log(`server is running on port : ${port}`)
+})
